ad0c12974e31be7894b87206d76e1ae2daf52016
Junaid Nadeem <junaidnadeem6725@gmail.com>
2026-02-06 17:48:02 +0500

    Add Vercel.json

--- server/index.js
+++ server/index.js
@@ -15,8 +15,15 @@ const allowOrigins = [process.env.FRONTEND_URL];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowOrigins, credentials: true }));
+// app.use(cors({ origin: allowOrigins, credentials: true }));
 app.use(express.urlencoded({ extended: false }));
+app.options(
+  "*",
+  cors({
+    origin: allowOrigins,
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>API Working</h1>");
