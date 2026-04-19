289d85bea40b2220e869aa5879ccaa15b7dc23b0
Junaid Nadeem <junaidnadeem6725@gmail.com>
2026-04-19 11:31:09 +0500

    Changes in index.js file

--- server/index.js
+++ server/index.js
@@ -11,12 +11,14 @@ dotenv.config();
 // const PORT = process.env.PORT || 8000;
 connectDB();
 
-app.use(
-  cors({
-    origin: "https://mern-auth-five-black.vercel.app",
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
